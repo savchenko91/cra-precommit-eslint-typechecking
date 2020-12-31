6377af378c4f3eee27e9b066aac48ceee8417844
savchenko alexander <savchenkoalexander91@gmail.com>
2020-12-31 15:16:59 +0400

    first commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { execSync } = require("child_process");
+
+class Manager {
+  filesArr = [];
+
+  precommit = false;
+
+  setPrecommit = (b) => {
+    this.precommit = b;
+  };
+
+  buildRegexp = (regexp) => {
+    if (!this.precommit) {
+      return regexp;
+    }
+
+    const filesBinary = execSync(
+      "echo $(git diff --cached --name-only --diff-filter=ACM)"
+    );
+
+    this.filesArr = filesBinary
+      ?.toString()
+      .replace(/(\r\n|\n|\r)/gm, "")
+      .split(" ")
+      .filter((f) => regexp.test(f));
+
+    return new RegExp(this.filesArr.join("|"));
+  };
+}
+
+const manager = new Manager();
+
+module.exports = manager;
